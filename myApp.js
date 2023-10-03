e9591bf7199d211936d5294fd0b4182e1805bd3b
Radhika Gupte <gupter@oregonstate.edu>
2023-10-03 12:51:37 -0700

    mounted middleware to a path with app.use function to seve a static asset

--- myApp.js
+++ myApp.js
@@ -7,8 +7,12 @@ console.log('Hello World');
 //   res.send('Hello Express');
 // });
 
+// Serving static assets
+app.use('/public', express.static(__dirname + '/public'));
+
 // Serving an HTML file
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+
 module.exports = app;
